f2cc25048b138d47cf86c982924c294df44764c4
Rachel Callaghan <rachelcallaghan81@gmail.com>
2019-10-08 10:11:27 +0100

    Add DAO connecter
    Adds DAO from old file, not changed for postgres

--- Server/src/DAOs/MysqlDao.java
+++ Server/src/DAOs/MysqlDao.java
@@ -5,10 +5,56 @@
  */
 package DAOs;
 
+import Exceptions.DaoException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 /**
  *
  * @author Administrator
  */
 public class MysqlDao {
-    
+    public Connection getConnection() throws DaoException 
+    {
+
+        String driver = "com.psql.jdbc.Driver";
+        String url = "jdbc:mysql://localhost:3306/movies";
+        String username = "root";
+        String password = "";
+        Connection con = null;
+        
+        try 
+        {
+            Class.forName(driver);
+            con = DriverManager.getConnection(url, username, password);
+        } 
+        catch (ClassNotFoundException ex1) 
+        {
+            System.out.println("Failed to find driver class " + ex1.getMessage());
+            System.exit(1);
+        } 
+        catch (SQLException ex2) 
+        {
+            System.out.println("Connection failed " + ex2.getMessage());
+            System.exit(2);
+        }
+        return con;
+    }
+
+    public void freeConnection(Connection con) throws DaoException 
+    {
+        try 
+        {
+            if (con != null) 
+            {
+                con.close();
+                con = null;
+            }
+        } 
+        catch (SQLException e) 
+        {
+            System.out.println("Failed to free connection: " + e.getMessage());
+            System.exit(1);
+        }
+    }   
 }
