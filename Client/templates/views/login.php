b7aaf6f628351025d4b8f6f6150344f80764a050
Conor Clarke <conorjamesclarke@gmail.com>
2019-10-09 11:49:13 +0100

    Commented out social media icons
    Commented out the social media icons.

--- Client/templates/views/login.php
+++ Client/templates/views/login.php
@@ -1,13 +1,16 @@
+<!-- Reference Bootstrap: https://bootsnipp.com/snippets/vl4R7 -->
+
+
 <div class="container">
 	<div class="d-flex justify-content-center h-100">
 		<div class="card">
 			<div class="card-header">
 				<h3>Sign In</h3>
-				<div class="d-flex justify-content-end social_icon">
+				<!-- <div class="d-flex justify-content-end social_icon">
 					<span><i class="fab fa-facebook-square"></i></span>
 					<span><i class="fab fa-google-plus-square"></i></span>
 					<span><i class="fab fa-twitter-square"></i></span>
-				</div>
+				</div> -->
 			</div>
 			<div class="card-body">
 				<form>
