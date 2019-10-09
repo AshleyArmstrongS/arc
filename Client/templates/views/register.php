0c3d6b65d38723ffc13ea9952e5ecbfee6ea5609
Conor Clarke <conorjamesclarke@gmail.com>
2019-10-09 11:49:59 +0100

    Added Register View and Controller
    Added in a register view and controller, view uses bootstrap
    html and the css from the login page.

---
+++ Client/templates/views/register.php
@@ -0,0 +1,138 @@
+<!-- Reference Bootstrap: https://bootsnipp.com/snippets/9Zxl -->
+
+<div class="container">
+    <div class="d-flex justify-content-center h-100">
+		<div class="card">
+            <div class="panel-heading">
+                <div class="card-header">
+                    <h3 class="panel-title">Please sign up for GoCollege</h3>
+                </div>
+            </div>
+                <div class="card-body">
+                <form role="form">
+                    
+                    <div class="input-group form-group">
+                        <input type="email" name="email" id="email" class="form-control input-sm" placeholder="Email">
+                    </div>
+                    <div class="row">
+                        <div class="col-xs-6 col-sm-6 col-md-6">
+                            <div class="form-group">
+                                <input type="text" name="first_name" id="first_name" class="form-control" placeholder="First Name">
+                            </div>
+                        </div>
+                        <div class="col-xs-6 col-sm-6 col-md-6">
+                            <div class="form-group">
+                                <input type="text" name="last_name" id="last_name" class="form-control" placeholder="Last Name">
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <input type="email" name="email" id="email" class="form-control input-sm" placeholder="Email Address">
+                    </div>
+
+                   
+                        
+                    <div class="form-group">
+                        <input type="password" name="password" id="password" class="form-control input-sm" placeholder="Password">
+                    </div>
+                
+                
+                    <div class="form-group">
+                        <input type="password" name="password_confirmation" id="password_confirmation" class="form-control input-sm" placeholder="Confirm Password">
+                    </div>
+                        
+                    <div class="form-group">
+                        <input type="text" name="starting_location" id="starting_location" class="form-control" placeholder="Starting Location">
+                    </div>
+
+                    <div class="form-group">
+                        <input type="text" name="college" id="college" class="form-control" placeholder="College">
+                    </div>
+                    <!-- 
+
+                     -->
+                    
+                    <input type="submit" value="Register" class="btn btn-info btn-block">
+                
+                </form>
+                </div>
+	    		</div>
+    		</div>
+    	</div>
+    </div>
+
+
+
+
+
+
+<!-- 
+
+<div class="row">
+<div class="col-sm-1"></div>
+<div class="col-sm-9">
+    <div class="card text-white bg-info mb-3">
+      <div class="card-body">
+        <h5 class="card-title">Welcome! Please fill out our sign up form</div></h5>
+        <p class="card-text">
+        <div class="register">
+        <form id='signup_form' action='' method='post'>
+
+            <div class="col-sm-10">
+                <label for='email'>Email:</label>
+                <input type='text' id='email' name='email' value='' class="form-control">
+            </div>
+
+            <div class="col-sm-10">
+                <label for='first_name'>First Name:</label>
+                <input type='text' id='first_name' name='first_name' value='' class="form-control">
+            </div>
+
+            <div class="col-sm-10">
+                <label for='last_name'>Last Name:</label>
+                <input type='text' id='last_name' name='last_name' value='' class="form-control">
+            </div>
+
+            <div class="col-sm-10">
+                <label class="radio inline"> 
+                    <input type="radio" name="gender" value="male" checked>
+                    <span>Male</span> 
+                </label>
+                <label class="radio inline"> 
+                    <input type="radio" name="gender" value="female">
+                    <span>Female</span> 
+                </label>
+            </div>
+            
+            <div class="col-sm-5">
+                <label for='password'>Password:</label>
+                <input type='password' id='password' name='password' value='' class="form-control">
+            </div>
+
+            <div class="col-sm-5">
+                <label for='confirm_password'>Confirm Password:</label>
+                <input type='password' id='confirm_password' name='confirm_password' value='' class="form-control">
+            </div>
+
+            <div class="col-sm-10">
+                <label for='starting_location'>Starting Location:</label>
+                <input type='text' id='starting_location' name='starting_location' value='' class="form-control">
+            </div>
+
+            <div class="col-sm-10">
+                <label for='college'>College:</label>
+                <input type='text' id='college' name='college' value='' class="form-control">
+            </div>
+            <p><br/></p>
+            <div class="row">
+                <div class="col-sm-11">
+                    <input style="float:right;" type='submit' value='Done' class="btn btn-success btn-lg">
+                </div>
+            </div>
+        </form>
+        </div>
+       </p>
+      </div>
+    </div>
+</div> -->
