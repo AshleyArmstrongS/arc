0785cb2a53ec9c124b5b58748f93c8fcb40d6f76
Conor Clarke <conorjamesclarke@gmail.com>
2019-10-11 15:05:40 +0100

    Added in name and value to input
    Added in the name and value attribute to the input so that they
    can be used in the controllers.

--- Client/templates/views/login.php
+++ Client/templates/views/login.php
@@ -18,14 +18,14 @@
 						<div class="input-group-prepend">
 							<span class="input-group-text"><i class="fas fa-user"></i></span>
 						</div>
-						<input type="text" class="form-control" placeholder="username">
+						<input type="text" class="form-control" name='username' value='<?= $username ?>' placeholder="username">
 						
 					</div>
 					<div class="input-group form-group">
 						<div class="input-group-prepend">
 							<span class="input-group-text"><i class="fas fa-key"></i></span>
 						</div>
-						<input type="password" class="form-control" placeholder="password">
+						<input type="password" class="form-control" name='password' value='<?= $password ?>' placeholder="password">
 					</div>
 					<div class="row align-items-center remember">
 						<input type="checkbox">Remember Me
