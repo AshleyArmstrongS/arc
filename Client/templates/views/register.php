2982274d0d42cce595f7faa15a635f8b869f5093
Aisling Cunningham <Aislingc98@icloud.com>
2019-11-04 11:02:35 +0000

    Change register  files after merge conflict
    Changed register files again in new branch due to merge commit.

--- Client/templates/views/register.php
+++ Client/templates/views/register.php
@@ -8,6 +8,9 @@
                     <h3 class="panel-title">Please sign up for GoCollege</h3>
                 </div>
             </div>
+            
+            <div class="card-body">
+                <form id='signup_form' action='' method='post'>
             <div class="alert alert-warning alert-dismissible" role="alert">
             <button type="button" class="close" data-dismiss="alert" aria-label="Close">
             <span aria-hidden="true">&times;</span>
@@ -16,46 +19,53 @@
             <p><?= $errors ?></p>
             <?php } ?>
             </div>
-                <div class="card-body">
-                <form id='signup_form' action='' method='post'>
-                    
-                    <div class="input-group form-group">
-                        <input type="email" name="email" id="email" class="form-control input-sm" placeholder="Email">
-                    </div>
-                    <div class="row">
-                        <div class="col-xs-6 col-sm-6 col-md-6">
-                            <div class="form-group">
-                                <input type="text" name="first_name" id="first_name" class="form-control" placeholder="First Name">
-                            </div>
-                        </div>
-                        <div class="col-xs-6 col-sm-6 col-md-6">
-                            <div class="form-group">
-                                <input type="text" name="last_name" id="last_name" class="form-control" placeholder="Last Name">
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="form-group">
+                <div class="input-group form-group">
                         <input type="email" name="email" id="email" class="form-control input-sm" placeholder="Email Address">
                     </div>
 
-                   
-                        
-                    <div class="form-group">
-                        <input type="password" name="password" id="password" class="form-control input-sm" placeholder="Password">
+                    <div class="input-group form-group">
+                        <input type="name" name="name" id="name" class="form-control input-sm" placeholder="Full Name">
                     </div>
-                
-                
-                    <div class="form-group">
-                        <input type="password" name="password_confirmation" id="password_confirmation" class="form-control input-sm" placeholder="Confirm Password">
-                    </div>
-                        
-                    <div class="form-group">
-                        <input type="text" name="starting_location" id="starting_location" class="form-control" placeholder="Starting Location">
+                    
+                    <div class="row">
+                        <div class="col-xs-6 col-sm-6 col-md-6">
+                            <div class="form-group">
+                                <input type="text" name="password1" id="password" class="form-control" placeholder="Password">
+                            </div>
+                        </div>
+                        <div class="col-xs-6 col-sm-6 col-md-6">
+                            <div class="form-group">
+                                <input type="text" name="password2" id="password2" class="form-control" placeholder="Confirm Password">
+                            </div>
+                        </div>
                     </div>
 
                     <div class="form-group">
-                        <input type="text" name="college" id="college" class="form-control" placeholder="College">
+                        <input type="text" name="college" id="college" class="form-control input-sm" placeholder="Name Of College">
+                    </div>
+                    <div class="form-group">
+                        <input type="text" name="descritpion" id="descritpion" class="form-control input-sm" placeholder="Description of yourself">
+                    </div>
+
+                    <div class="row">
+                        <div class="col-xs-6 col-sm-6 col-md-6">
+                            <div class="form-group">
+                                <input type="text" name="age" id="age" class="form-control" placeholder="Age">
+                            </div>
+                        </div>
+                        <div class="col-xs-6 col-sm-6 col-md-6">
+                            <div class="form-group">
+                                <input type="text" name="gender" id="gender" class="form-control" placeholder="Female(F) or Male(M)">
+                            </div>
+                        </div>
+                    </div>
+                        
+                    <div class="form-group">
+                        <input type="text" name="starting_location" id="starting_location" class="form-control" placeholder="Starting Address">
+                    </div>
+
+                    <div class="form-group">
+                        <input type="text" name="userType" id="userType" class="form-control" placeholder="Passenger (P) or Driver(D)">
                     </div>
                     <!-- 
 
@@ -69,6 +79,3 @@
     		</div>
     	</div>
     </div>
-
-
-
