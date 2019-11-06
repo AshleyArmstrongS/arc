df44130edaecfa92b24f1e71864bd223b6cfada8
Aisling Cunningham <Aislingc98@icloud.com>
2019-11-06 09:56:05 +0000

    Create two options for reister
    Craeted two options for register, need to create two register forms.

--- Client/templates/views/register.php
+++ Client/templates/views/register.php
@@ -12,9 +12,6 @@
             <div class="card-body">
                 <form id='signup_form' action='' method='post'>
             <div class="alert alert-warning alert-dismissible" role="alert">
-            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-            </button>
             <?php foreach($locals['form_error_messages'] as $errors) { ?>
             <p><?= $errors ?></p>
             <?php } ?>
