34e59c8dcb1a61615d42450a94fd27afe9a65bb9
AshleyArmstrongS <D00213195@student.dkit.ie>
2019-11-11 15:58:50 +0000

    create the base files for the messaging messaging_system
    this includes the inbox as it is vital to messaging
    includes views controllers get/posts

---
+++ Client/templates/views/Inbox.php
@@ -0,0 +1,11 @@
+<!-- Reference Bootstrap: https://bootsnipp.com/snippets/vl4R7 -->
+<div class="container">
+	<div class="d-flex justify-content-center h-100">
+		<div class="card">
+			<div class="card-header">
+				<h3>Sign In</h3>
+                
+            </div>
+        </div>
+    </div>
+</div>
