818231d41ca8b65bf838cace9a4f42147a0d1b2e
AshleyArmstrongS <D00213195@student.dkit.ie>
2019-11-14 10:33:18 +0000

    Work on the queries for inbox

--- Client/models/Message.php
+++ Client/models/Message.php
@@ -19,7 +19,7 @@ class Message {
         $this->from_id          = $args['from_id'] ?? NULL;
         $this->time_sent        = $args['time_sent'] ?? NULL;
         $this->message          = $args['message'] ?? NULL;
-
+    }
         
         public function getMessage_id()
         {
@@ -45,9 +45,14 @@ class Message {
         {
             return $this->message;
         }
-    
-
-
+        public function setMessage_id($message_id)
+        {
+            if ($message_id === NULL) {
+                $this->message_id = NULL;
+                return;
+            }
+            $this->message_id = $message_id;
+        }
         public function setTo_id($to_id)
         {
             if ($to_id === NULL) {
@@ -84,5 +89,55 @@ class Message {
             }
             $this->message = $message;
         }
+        public function submitMessage($message, $db)
+        {
+            if($message->getMessage() === NULL)
+            {
+            throw new Exception('Cannot submit a message without contents');
+            }
+            $statement = $db->prepare('INSERT into users (to_id, recipients_ids) VALUES ();');
+            $statement->excecute([
+                
+                'to_id'         => $message->getTo_id(),
+                'recipient_ids' => $message->getRecipient_ids(),
+                'from_id'       => $message->getFrom_id(),
+                'time_sent'     => $message->getTime_sent(),
+                'message'       => $message->getMessage()
+            ]);
+            $saved = $statement->rowCount() === 1;
+
+            if ($saved) {
+                $message->setMessage_id($db->lastInsertId());
+            }
+            return $saved;
+        }
+        public function getInboxByUser_id($user_id, $db)
+        {
+            $user_id = (int)$user_id;
+            $query1 = $db->prepare('SELECT to_id, user_id, WHERE $user_id = :user_id';);
+            $query1->execute([
+                'user_id' => $user_id;
+            ]);
+            $query2 = $db->prepare('SELECT , , WHERE $user_id = :user_id ';);
+            $query2->execute([
+                'user_id' => $user_id;
+            ]);
+
+            $inbox = array_push($query1->fetchAll());
+            
+            return $inbox;
+        }
+        public function getMessagesByTo_id($to_id, $db)
+        {
+            $to_id = (int)$to_id;
+
+            $query = $db->prepare('SELECT * FROM message WHERE to_id = :to_id';);
+            $query->execute([
+                'to_id' => $to_id
+            ]);
+
+            $messages = $query->fetchAll();
+            return $messages;
+        }
     }
-}
+?>
