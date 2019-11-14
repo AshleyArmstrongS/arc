15a3ee09d997ce729eb94b65be957210d2ff188f
Conor Clarke <conorjamesclarke@gmail.com>
2019-11-14 12:15:00 +0000

    Added Info
    Added comments for the info that will be needed for the view.

--- Client/controllers/MessagePost.php
+++ Client/controllers/MessagePost.php
@@ -4,4 +4,16 @@
   require('./models/User.php');
   $db = \Rapid\Database::getPDO();
   
-}
+  // get the groups [groups]
+  // get the names of the persons [p1,p2,p3,...pn] in each group
+  // get the last message of that chat
+  // get the date
+
+
+
+  $res->render('main', 'message', [
+    'pageTitle' => 'Message',
+    'messages' => $messages
+  ]);
+
+} ?>
