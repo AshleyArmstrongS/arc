abeca0af3a5c57029628f5e12ed9990e018d75fa
AshleyArmstrongS <D00213195@student.dkit.ie>
2019-11-14 16:16:19 +0000

    Merge conflicts

--- Client/models/Message.php
+++ Client/models/Message.php
@@ -91,36 +91,9 @@ class Message {
                 $message->setMessage_id($db->lastInsertId());
                 return TRUE;
             }
-<<<<<<< HEAD
             return $saved;
         }
-        public function getUsersByGroup_id($group_id, $db)
-        {
-            $group_id = (int)$group_id;
-
-            
-            $query1 = $db->prepare('SELECT user_id, group_id from userspergroup where group_id = :group_id;');
-            $query1->execute([
-                'group_id' => $group_id
-            ]);
-            $inbox = array_push($query1->fetchAll());
-            
-            return $inbox;
-        }
-        public function getGroupsByUser_id($user_id, $db)
-        {
-            $user_id = (int)$user_id;
-
-            
-            $query1 = $db->prepare('SELECT group_id, user_id from userspergroup where user_id = :user_id;');
-            $query1->execute([
-                'user_id' => $user_id
-            ]);
-            $inbox = $query1->fetchAll();
-=======
->>>>>>> messaging_system_functions
-            
-        }
+        
         public function getMessagesByGroup_id($group_id, $db)
         {
             $group_id = (int)$group_id;
