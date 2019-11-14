ebda09cac1777f5010ed50abb0102d8b37665e38
AshleyArmstrongS <D00213195@student.dkit.ie>
2019-11-14 16:07:42 +0000

    seperate the message into message and group as this makes more sense
    add the group model to handle the inbox better

---
+++ Client/models/Group.php
@@ -0,0 +1,109 @@
+<<?php require_once('Model.php'); ?>
+<?php
+
+class Group {
+    private $group_id;
+    private $admin_id;
+    private $recipients_ids;
+
+    public function __construct($args) {
+        if(!is_array($args)) {
+            throw new Exception('User constructor requires an array');
+        }
+        $this->group_id         = $args['group_id'] ?? NULL;
+        $this->recipient_ids    = $args['recipient_ids'] ?? NULL;
+        $this->admin_id          = $args['admin_id'] ?? NULL;
+    }
+    public function getGroup_id()
+        {
+            return $this->group_id;
+        }
+        public function getRecipient_ids()
+        {
+            return $this->recipient_ids;
+        }
+        public function getAdmin_id()
+        {
+            return $this->admin_id;
+        }
+
+        public function setGroup_id($group_id)
+        {
+            if ($group_id === NULL) {
+                $this->to_igroup_idd = NULL;
+                return;
+            }
+            $this->group_id = $group_id;
+        }
+        public function setRecipient_ids($recipient_ids)
+        {
+            if ($recipient_ids === NULL) {
+                $this->recipient_ids = NULL;
+                return;
+            }else if(!is_array($recipient_ids))
+            {
+                $this->recipient_ids = NULL;
+                return; 
+            }
+            $this->recipient_ids = $recipient_ids;
+        }
+        public function setAdmin_id($admin_id)
+        {
+            if ($admin_id === NULL) {
+                $this->admin_id = NULL;
+                return;
+            }
+            $this->admin_id = $admin_id;
+        }
+        public function addGroup($group, $db)
+        {
+            $statement = $db->prepare('INSERT into groups (admin_id) VALUES(:admin_id)')
+            $statement->execute([
+                'admin_id' => $$group->getAdmin_id()
+            ]);
+            $saved = $statement->rowCount() === 1;
+
+            if ($saved) {
+                $group->setGroup_id($db->lastInsertId());
+            }
+            $recipient_ids_temp = $group->getRecipient_ids();
+            foreach($recipient_ids_temp['user_id'] as $recipient){
+                $statement2 = $db->prepare('INSERT into userspergroup (group_id, user_id) VALUES(:group_id, :user_id)')
+                $statement2->execute([
+                    'admin_id' => $group->getAdmin_id(),
+                    'user_id' => $recipient
+                ]);
+            }
+            $saved = $statement->rowCount() === size_of($recipient_ids_temp);
+        if()
+        {
+
+        }
+        }
+        public function getUsersByGroup_id($group_id, $db)
+        {
+            $group_id = (int)$group_id;
+
+            $query = $db->prepare('SELECT u.user_id, u.name, p.group_id from userspergroup p inner join users u on p.user_id = u.user_id where p.group_id = :group_id;'););
+            $query->execute([
+                'group_id' => $group_id;
+            ]);
+            $users = array_push($query->fetchAll());
+            
+            return $users;
+        }
+        
+        public function getGroupsByUser_id($user_id, $db)
+        {
+            $user_id = (int)$user_id;
+
+            
+            $query = $db->prepare('SELECT p.group_id, p.user_id, g.admin_id from userspergroup p inner join groups g on p.group_id = g.group_id where p.user_id = :user_id;'););
+            $query->execute([
+                'user_id' => $user_id;
+            ]);
+            $inbox = array_push($query->fetchAll());
+            
+            return $inbox;
+        }
+        
--- Client/models/Message.php
+++ Client/models/Message.php
@@ -4,7 +4,6 @@
 class Message {
     private $message_id;
     private $group_id;
-    private $recipient_ids;
     private $from_id;
     private $time_sent;
     private $message;
@@ -14,8 +13,7 @@ class Message {
             throw new Exception('User constructor requires an array');
         }
         $this->message_id       = $args['message_id'] ?? NULL;
-        $this->group_id            = $args['group_id'] ?? NULL;
-        $this->recipient_ids    = $args['recipient_ids'] ?? NULL;
+        $this->group_id         = $args['group_id'] ?? NULL;
         $this->from_id          = $args['from_id'] ?? NULL;
         $this->time_sent        = $args['time_sent'] ?? NULL;
         $this->message          = $args['message'] ?? NULL;
@@ -28,10 +26,6 @@ class Message {
         public function getGroup_id()
         {
             return $this->group_id;
-        }
-        public function getRecipient_ids()
-        {
-            return $this->recipient_ids;
         }
         public function getFrom_id()
         {
@@ -61,18 +55,6 @@ class Message {
             }
             $this->group_id = $group_id;
         }
-        public function setRecipient_ids($recipient_ids)
-        {
-            if ($recipient_ids === NULL) {
-                $this->recipient_ids = NULL;
-                return;
-            }else if(!is_array($recipient_ids))
-            {
-                $this->recipient_ids = NULL;
-                return; 
-            }
-            $this->recipient_ids = $recipient_ids;
-        }
         public function setFrom_id($from_id)
         {
             if ($from_id === NULL) {
@@ -95,10 +77,10 @@ class Message {
             {
             throw new Exception('Cannot submit a message without contents');
             }
-            $statement = $db->prepare('INSERT into users (group_id, recipients_ids, from_id, time_sent, message) VALUES (:group_id, :from_id, :time_sent, :message);');
+            $statement = $db->prepare('INSERT into users (group_id, from_id, time_sent, message) VALUES (:group_id, :from_id, :time_sent, :message);');
             $statement->excecute([
                 
-                'group_id'         => $message->getGroup_id(),
+                'group_id'      => $message->getGroup_id(),
                 'from_id'       => $message->getFrom_id(),
                 'time_sent'     => $message->getTime_sent(),
                 'message'       => $message->getMessage()
@@ -107,41 +89,15 @@ class Message {
 
             if ($saved) {
                 $message->setMessage_id($db->lastInsertId());
+                return TRUE;
             }
-            return $saved;
-        }
-        public function getUsersByGroup_id($group_id, $db)
-        {
-            $group_id = (int)$group_id;
-
             
-            $query1 = $db->prepare('SELECT user_id, group_id from userspergroup where group_id = :group_id;'););
-            $query1->execute([
-                'group_id' => $group_id;
-            ]);
-            $inbox = array_push($query1->fetchAll());
-            
-            return $inbox;
-        }
-        public function getGroupsByUser_id($user_id, $db)
-        {
-            $user_id = (int)$user_id;
-
-            
-            $query1 = $db->prepare('SELECT group_id, user_id from userspergroup where user_id = :user_id;'););
-            $query1->execute([
-                'user_id' => $user_id;
-            ]);
-            $inbox = array_push($query1->fetchAll());
-            
-            return $inbox;
         }
         public function getMessagesByGroup_id($group_id, $db)
         {
             $group_id = (int)$group_id;
 
-            $query = $db->prepare('SELECT * from messages m inner join groups g on m.to_id = g.group_id where g.group_id = :group_id;
-            ';);
+            $query = $db->prepare('SELECT * from messages m inner join groups g on m.to_id = g.group_id where g.group_id = :group_id;');
             $query->execute([
                 'group_id' => $group_id
             ]);
