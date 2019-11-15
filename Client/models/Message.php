0eec8f388b6d89896295cd46db4000cc6b257efd
Conor Clarke <conorjamesclarke@gmail.com>
2019-11-15 15:37:04 +0000

    GetLastMessage Function now returns message object

--- Client/models/Message.php
+++ Client/models/Message.php
@@ -113,16 +113,16 @@ class Message {
     {
         $group_id = (int)$group_id;
 
-        $query = $db->prepare('SELECT m.message FROM messages m  INNER JOIN ( SELECT MAX(message_id) AS max_id FROM messages) max ON m.message_id = max.max_id where m.to_id =   :group_id;
-        ');
+        $query = $db->prepare('SELECT m.* FROM messages m  INNER JOIN (SELECT MAX(message_id) AS max_id FROM messages) max ON m.message_id = max.max_id where m.to_id = :group_id;');
         $query->execute([
             'group_id' => $group_id
         ]);
 
 
-            $message = $query->fetch();
-            
-            return $message !== FALSE ?? FALSE;
+        $message = $query->fetch();
+        
+        //return $message;// !== FALSE ?? FALSE;
+        return $message !== FALSE ? new Message($message) : NULL;
         }
     }
     
