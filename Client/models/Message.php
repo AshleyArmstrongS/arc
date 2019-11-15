aa6be9fb25fd27aa51ab43b5c14c8eb007fcc4eb
AshleyArmstrongS <D00213195@student.dkit.ie>
2019-11-15 11:46:14 +0000

    Edit the last message to match the message table in the db
    the message table in the db uses a table to_id but everywhere else is
    group_id

--- Client/models/Message.php
+++ Client/models/Message.php
@@ -111,7 +111,7 @@ class Message {
     {
         $group_id = (int)$group_id;
 
-        $query = $db->prepare('SELECT m.message FROM messages m  INNER JOIN ( SELECT MAX(message_id) AS mi FROM messages) max_id ON m.message_id = max_id.mi where m.group_id = :group_id;
+        $query = $db->prepare('SELECT m.message FROM messages m  INNER JOIN ( SELECT MAX(message_id) AS max_id FROM messages) max ON m.message_id = max.max_id where m.to_id = :group_id;
         ');
         $query->execute([
             'group_id' => $group_id
