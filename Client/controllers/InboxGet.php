44f10538ed79b53d445f7e8b8a43d12dbdea0245
Conor Clarke <conorjamesclarke@gmail.com>
2019-11-18 10:20:09 +0000

    Added a session
    Added a session to get the user email so that you can then get
    the user object from the database.

--- Client/controllers/InboxGet.php
+++ Client/controllers/InboxGet.php
@@ -4,27 +4,21 @@
  
  require('./models/Group.php');
  require('./models/User.php');
+ $req->sessionStart();
  $db = \Rapid\Database::getPDO();
  
- // get the groups [groups]
- // get the names of the persons [p1,p2,p3,...pn] in each group
- // get the last message of that chat
- // get the date
- $user_id = 1;
- $usersGroup = array();
+ $user_email = $_SESSION['Email'];
+ $user_id = User::getUserByEmail($user_email, $db);
  $messageInfo = array();
  $group_messages = array();
  $last_user_with_message = array();
  $user_ids= array();
- $userInGroup = Group::getGroupsByUser_id($user_id, $db);
+ $userInGroup = Group::getGroupsByUser_id($user_id->getUser_id(), $db);
  //print_r($userGroups);
 
-  foreach ($userInGroup as $userGroup) {
+   foreach ($userInGroup as $userGroup) {
       array_push($group_messages, Message::getLastMessagesByGroup_id($userGroup['group_id'], $db));
-      $usersGroup= Group::getUsersByGroup_id($userGroup['group_id'], $db);
-      $messageInfo = Message::getMessagesByGroup_id($userGroup['group_id'], $db);
    }
-
 
    foreach ($group_messages as $message) {
       array_push($user_ids, $message->getFrom_id());
@@ -36,7 +30,6 @@
    
    $res->render('main', 'inbox', [
    'pageTitle' => 'Inbox',
-   'group_users' => $usersGroup,
    'message_info' => $group_messages,
    'user' => $last_user_with_message
    ]);
