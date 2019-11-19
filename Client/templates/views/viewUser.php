1c3d2d2c75cecf0b0eb28f92e661e87b3d3d2246
Aisling Cunningham <Aislingc98@icloud.com>
2019-11-19 13:10:58 +0000

    Search box added
    This search function allows the user to type in a name and then click
    the search button. This will return the users that match the name.

--- Client/templates/views/viewUser.php
+++ Client/templates/views/viewUser.php
@@ -1,8 +1,18 @@
 
-<h2>Users</h2>
-<div class="row" style="margin-top: 5px;">
+
+<div class="card-body">
+<div class="row">
+<div class="col-xs-3 col-sm-3 col-md-3">
+            <form action='search'>
+            <div class="input-group form-group">
+            <input type="search" name="search" class="form-control input-sm" placeholder="Name">
+            </div>
+            <div class="col-sm-6">
+            <input type="submit" value="search"  class="btn btn-info btn-block">
+
+</div>
     <?php foreach ($locals['viewUsers'] as $user) { ?>
-        <div class="col-sm-4">
+        <div class="col-sm-12">
             <div class="card">
                 <div class="card-body">
                     <h5 class="card-title"> <?= $user->getName(); ?></h5>
@@ -12,3 +22,4 @@
         </div>
     <?php } ?>
 </div>
+</form>
