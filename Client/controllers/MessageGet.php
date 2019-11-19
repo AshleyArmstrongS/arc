2c4e10e25c5d89e83c7a22c3135a1e0a2944788d
AshleyArmstrongS <D00213195@student.dkit.ie>
2019-11-19 09:31:20 +0000

    Work on the simple post message system for initial release

--- Client/controllers/MessageGet.php
+++ Client/controllers/MessageGet.php
@@ -1,12 +1,14 @@
 <?php return function($req, $res) {
  require('./lib/FormUtils.php');
  require('./models/Message.php');
+ require('./models/Group.php');
  require('./models/User.php');
+ $req->sessionStart();
  $db = \Rapid\Database::getPDO();
 
  $group_id = 1;
- $messages = Message::getMessagesByGroup($group_id, $db);
- $users = Users::ByUsersGroup_id();
+ $messages = Message::getMessagesByGroup_id($group_id, $db);
+ $users = Group::getUsersByGroup_id($group_id, $db);
 
 
  $res->render('main', 'message', [
@@ -14,3 +16,5 @@
   'messages' => $messages,
   'users' => $users
   ]);
+ }
+ ?>
