c2e084c21b48885ab63480cbc22f10aae8401a05
Conor Clarke <conorjamesclarke@gmail.com>
2019-11-20 11:00:43 +0000

    Updated the view
    View now displays within a chatbox.

--- Client/templates/views/message.php
+++ Client/templates/views/message.php
@@ -1,5 +1,5 @@
 <style>
-#messages {
+/* #messages {
   font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
   border-collapse: collapse;
   width: 100%;
@@ -13,7 +13,7 @@
 #messages tr{background-color: #f2f2f2;}
 
 /* #messages tr:hover {background-color: #ddd;} */
-
+/* 
 #messages th {
   padding-top: 10px;
   padding-bottom: 10px;
@@ -24,52 +24,108 @@
 
 #message {
     color: blue;
+}  */
+</style>
+
+<style>
+/* Reference - https://code.tutsplus.com/tutorials/how-to-create-a-simple-web-based-chat-application--net-5931 */
+body {
+    font:10px arial;
+    color: #222;
+    text-align:left;
+    padding:35px; }
+  
+p {
+    margin:0;
+    padding:0; }
+  
+input { 
+    font:12px arial; 
+
 }
+  
+a {
+    color:#0000FF;
+    text-decoration:none; 
+}
+  
+a:hover { 
+    text-decoration:underline; 
+}
+  
+#wrapper, #loginform {
+    margin:0 auto;
+    padding-bottom:25px;
+    background:#EBF4FB;
+    width:504px;
+    border:1px solid #ACD8F0; 
+}
+  
+#chatbox {
+    text-align:left;
+    margin:0 auto;
+    margin-bottom:25px;
+    padding:10px;
+    background:#fff;
+    height:400px;
+    width:430px;
+    border:1px solid #ACD8F0;
+    overflow:auto; 
+}
+  
+#message {
+    width: 370px;
+    margin-left: 35px;
+    border:1px solid #ACD8F0; 
+}
+  
+#group_id { 
+  width: 60px; 
+}
+  
+#top { 
+    padding: 12.5px 25px 12.5px 25px; 
+}
+  
+
+
 </style>
 
 
 <h1>Messages</h1>
 
-<table id="messages">
-    <tr>
-        <th>Name</th>
-        <th>Message</th>
-        <th>Time Sent</th>
-        <th>Delete</th>
-    </tr>
-  
-
-    <?php 
-    $group_id = $locals['group_id'];
-    foreach ($locals['messages'] as $message) { 
-      ?>
-    <div class="row">  
-    <tr>
-        
+  <div id="wrapper">
+    <div id="top"></div>
+     
+      <div id="chatbox">
+      
+      <?php $group_id = $locals['group_id']; ?>
+      <?php foreach ($locals['messages'] as $message) { ?>
         <?php
-        if(strtotime($message['time_sent']) <= time() - (60*60*24)){
-            $regEx = '/(\d{4})-(\d{2})-(\d{2}) /';
-            preg_match($regEx, $message['time_sent'], $result);
-        }
-        else{
-            $regEx = '/ (\d{2}):(\d{2})/';
-            preg_match($regEx, $message['time_sent'], $result);
-        }
-       ?>
-        <td><div><?= $message['name']; ?></div></td>
-        <td id=message><div><?= $message['message']; ?></div></td>
-        <td><div><?= $result[0]; ?></div></td>
-        <td>
-        <?php  if($_SESSION['Id'] === $message['from_id'])
-        { ?>
-          <a href="/arc/Client/removeMessage?message_id= <?= $message['message_id'] ?>&to_id=<?=$message['to_id']?>"> delete</a>
-        <?php } ?>
-        </td>
-    </tr>
-    <?php } ?>  
-</table>
+          if(strtotime($message['time_sent']) <= time() - (60*60*24)){
+              $regEx = '/(\d{4})-(\d{2})-(\d{2}) /';
+              preg_match($regEx, $message['time_sent'], $result);
+          }
+          else{
+              $regEx = '/ (\d{2}):(\d{2})/';
+              preg_match($regEx, $message['time_sent'], $result);
+          }
+        ?>
 
-<form id='login_form' action='' method='post'>
-<input type="text" class="form-control" name='message' id='message' placeholder="Hi!">
-<input type="hidden" name="group_id" id="group_id" value = <?=$group_id?>>
-</form>
+          <p>
+            (<?= $result[0]; ?>) 
+            <i style="font-weight:bold; color:black;"><?= $message['name']; ?>: </i>
+            <?= $message['message']; ?> 
+            <?php  if($_SESSION['Id'] === $message['from_id']) { ?>
+              <a href="/arc/Client/removeMessage?message_id= <?= $message['message_id'] ?>&to_id=<?= $message['to_id']; ?>">Delete</a>
+            <?php } ?>
+          </p>
+      <?php } ?>
+      </div>
+
+      <form id='login_form' action='' method='post'>
+        <input type="text" name='message' id='message' placeholder="Enter message here...">
+        <input type="submit" name="group_id" id="group_id" value = 'Submit'>
+      </form>
+    </div>  
+  </div>  
