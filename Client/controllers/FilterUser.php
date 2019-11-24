e06c47e1b314d37d79634c8864406be57cbe7149
Aisling Cunningham <Aislingc98@icloud.com>
2019-11-24 21:13:49 +0000

    Added a link to users and error message
    Ensured that all possible scenarios of filtering by gender or by female
    is covered. Added a tags so they user can be messages and added an error
    message that will display if there is no users returned.

--- Client/controllers/FilterUser.php
+++ Client/controllers/FilterUser.php
@@ -3,26 +3,48 @@
 $req->sessionStart();
 $db = \Rapid\Database::getPDO();
 require('./models/User.php');
-$gender = $_POST["gender"];
-//$day = $_POST["check_list"];
+$gender = $_POST["gender"] ?? NULL;
+$day = $_POST["check_list"] ?? NULL;
 
-//$gender = $req->query('filter');
-$users = User::getDriversByGender($db, $gender);
-//$users = User::getDriversByDayAndGender($db,$day,$gender);
+if($gender != NULL && $_POST["check_list"] != NULL)
+{
+    
+    $day = $_POST["check_list"];
+    $users = User::getDriversByDayAndGender($db,$day,$gender);
+}
+else if($gender != NULL && $_POST["check_list"] == NULL)
+{
+    
+    $users = User::getDriversByGender($db, $gender);
+}
+else 
+{
+    $day = $_POST["check_list"];
+    $users = User::getDriversByDay($db,$day);
+}
 
-// $res->render('main', 'viewUser', [
-//     'pageTitle' => 'View Users',
-//     'viewUsers'  => $users
-
-// ]);
-foreach ($users as $user) { ?>
+if ($users == NULL) { ?>
     <div class="col-sm-12">
-        <div class="card">
-            <div class="card-body">
-                <h5 class="card-title"> <?= $user["name"]; ?> <i class="fas fa-comment-alt"></i> </h5>
+                        <div class="card">
+                            <div class="card-body">
+                                <h5 class="card-title">Unfortunately no users exist relating to your search.</h5>
+                            </div>
+                        </div>
+                    </div>
+<?php  }
+else {
+    foreach ($users as $user) {
+        ?>
+        <div class="col-sm-12">
+            <div class="card">
+                <div class="card-body">
+                <a href="/arc/Client/createGroup?recipient_id= <?= $user->getUser_id() ?>">
+                                            <h5 class="card-title"> <?= $user->getName(); ?> <i class="fas fa-comment-alt"></i> </h5>
+                </div>
             </div>
         </div>
-    </div>
-<?php } ?>
+    <?php } ?>
 
-<?php } ?>
+
+<?php }
+} ?>
