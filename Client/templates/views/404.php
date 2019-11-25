9cb9e352538933c9c8394b058e4bd61da053146e
Aisling Cunningham <Aislingc98@icloud.com>
2019-11-25 09:39:49 +0000

    Including error page
    Th epage will catch all pages that the user will try and access but
    doesnt exist.

---
+++ Client/templates/views/404.php
@@ -0,0 +1,17 @@
+
+<br>
+<br>
+<br>
+<br>
+<div>
+<h2 class="glyphicon glyphicon-exclamation-sign">   Sorry, the page requested can not be found!</h2 >
+</div>
+<div>
+<?php if ($_SESSION['LOGGED_IN'] === TRUE) { ?>
+<h3 class="glyphicon glyphicon-circle-arrow-right">  Go back to <a href='<?= SITE_BASE_DIR ?>/home'> Home </a></h3>
+<?php } ?>
+<div>
+<h3 class="glyphicon glyphicon-circle-arrow-right">  Go back to <a href='<?= SITE_BASE_DIR ?>/login'> Login </a></h3>
+</div>
+
+</div>
