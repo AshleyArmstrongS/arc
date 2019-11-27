6674163918367545a5b3dfa8d73b43107f2c62ec
Aisling Cunningham <Aislingc98@icloud.com>
2019-11-27 15:21:57 +0000

    Checks on each page to check if user is logged in
    If the user is logged in the particular pages will be rendered, if the
    user is not logged in then they will be redirected to 404 page. This is
    to prevent people accessing pages via the URL.

--- Client/controllers/MessageGet.php
+++ Client/controllers/MessageGet.php
@@ -1,22 +1,30 @@
+<?php error_reporting(E_ALL ^ E_NOTICE); ?>
 <?php return function($req, $res) {
- require('./lib/FormUtils.php');
- require('./models/Message.php');
- require('./models/Group.php');
- require('./models/User.php');
- $req->sessionStart();
- $db = \Rapid\Database::getPDO();
+
+    $req->sessionStart();
+
+    if ($_SESSION['LOGGED_IN'] === TRUE) {
+        require('./lib/FormUtils.php');
+        require('./models/Message.php');
+        require('./models/Group.php');
+        require('./models/User.php');
+        $db = \Rapid\Database::getPDO();
 
 
- $group_id = $req->query('to_id');
- $messages = Message::getMessagesByGroup_id($group_id, $db);
- $users = Group::getUsersByGroup_id($group_id, $db);
+        $group_id = $req->query('to_id');
+        $messages = Message::getMessagesByGroup_id($group_id, $db);
+        $users = Group::getUsersByGroup_id($group_id, $db);
 
 
-$res->render('main', 'message', [
-  'pageTitle' => 'message',
-  'messages' => $messages,
-  'group_id' => $group_id,
-  'users' => $users
-  ]);
- }
- ?>
+        $res->render('main', 'message', [
+            'pageTitle' => 'message',
+            'messages' => $messages,
+            'group_id' => $group_id,
+            'users' => $users
+        ]);
+    }
+    else {
+        $res->render('main', '404', []);
+    }
+}
+?>
