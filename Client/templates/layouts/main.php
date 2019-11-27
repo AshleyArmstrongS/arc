c85e2840ced5823dba567818bddde0717412cfa4
Conor Clarke <conorjamesclarke@gmail.com>
2019-11-27 22:45:39 +0000

    Added some styling
    Changed the current style to something different

--- Client/templates/layouts/main.php
+++ Client/templates/layouts/main.php
@@ -19,19 +19,39 @@
 
         <link rel="stylesheet" href="/arc/Client/assets/styles/styles.css">
 
+    <style>
+    
+    a {
+        color: #000;
+        text-decoration: none;
+    }
 
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    a:hover {
+        background-color: #999;
+        color: yellow;
+        border-radius: 4px;
+    }
+
+    ul {
+        list-style-type: none;
+        margin: 0;
+        padding: 0;
+        background-color: #f1f1f1;
+    }
+    
+    </style>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light" style="opacity: 0.5;">
         <div class="container">
-            <a class="navbar-brand" href="">GoCollege</a>
+            <div class="navbar-brand" href="">GoCollege</div>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample07" aria-controls="navbarsExample07" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
 
-            <div class="collapse navbar-collapse" id="navbarsExample07">
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav mr-auto">
                 <?php if ($_SESSION['LOGGED_IN'] === TRUE) { ?>
                     <li class="nav-item active">
-                        <a class="nav-link" href="<?= SITE_BASE_DIR ?>/home"><span class="fas fa-home"></span> Home</a>
+                        <a class="nav-link" href="<?= SITE_BASE_DIR ?>/home"><span class="sr-only">(current)</span> Home</a>
                     </li>
                     <li class="nav-item active">
                         <a class="nav-link" href="<?= SITE_BASE_DIR ?>/viewUser"><span class="fas fa-search-location"></span> Search</a>
@@ -45,7 +65,7 @@
                         <a class="nav-link" href="<?= SITE_BASE_DIR ?>/login"><span class = "fas fa-users"></span> Log in</a>
                     </li>
                     <li class="nav-item active">
-                        <a class="nav-link" href="<?= SITE_BASE_DIR ?>/userType"><span class = "fas fa-user-cog"></span>Register</a>
+                        <a class="nav-link" href="<?= SITE_BASE_DIR ?>/userType"><span class = "fas fa-user-cog"></span> Register</a>
                     </li>
                 <?php } ?>
 
