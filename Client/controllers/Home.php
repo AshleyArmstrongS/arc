bdca5420af05ff7b0bf3b2fb7fc2a09d53de8bd6
AshleyArmstrongS <D00213195@student.dkit.ie>
2019-11-28 19:38:30 +0000

    Change the way home works to allow for the use of the code in profile
    change the index to to just be client/ and the file to be called profile

--- Client/controllers/Home.php
+++ Client/controllers/Home.php
@@ -5,9 +5,11 @@ $req->sessionStart();
     
 if ($_SESSION['LOGGED_IN'] === TRUE) { 
     
- $res->render('main', 'Home', [
- 'pageTitle' => 'Home'
- 
+$db = \Rapid\Database::getPDO();
+$user = User::getUserByUser_ID($_SESSION['Id'], $db);
+ $res->render('main', 'profile', [
+ 'pageTitle' => 'Home',
+ 'user' => $user
  ]);
 }
 else{
