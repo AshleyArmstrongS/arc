4d81ec3616cd9e32031670fdd03cf7fddb68c41d
Rachel Callaghan <rachelcallaghan81@gmail.com>
2019-11-28 19:39:25 +0000

    Display Distance
    Display distance between filtered user and session user

--- Client/controllers/FilterUser.php
+++ Client/controllers/FilterUser.php
@@ -1,14 +1,14 @@
-<?php error_reporting (E_ALL ^ E_NOTICE); ?>
 <?php return function($req, $res) {
 
 $req->sessionStart();
-    
-if ($_SESSION['LOGGED_IN'] === TRUE) { 
 $db = \Rapid\Database::getPDO();
 require('./models/User.php');
+require('./models/Location.php');
 $gender = $_POST["gender"] ?? NULL;
 $day = $_POST["check_list"] ?? NULL;
 
+$currentUser = User::getUserByEmail($_SESSION['Email'], $db);
+$location_of_user = Location::returnLatLongById($db, $currentUser->getLocation());
 if($gender != NULL && $day != NULL)
 {
     $users = User::getDriversByDayAndGender($db,$day,$gender);
@@ -39,6 +39,7 @@ else {
                 <div class="card-body">
                 <a href="/arc/Client/createGroup?recipient_id=<?= $user["user_id"];?>">
                     <h5 class="card-title"> <?= $user["name"]; ?>  <i class="fas fa-comment-alt"></i> </h5>
+                    <h6> <?= Location::calculateDistance($db, $user["location_id"], $location_of_user[0], $location_of_user[1])[0] ?>  km away</h6>
                 </a>
                 </div>
             </div>
@@ -47,8 +48,4 @@ else {
 
 
 <?php }
-}
-}
-else{
-    $res->render('main', '404', []);
 } ?>
