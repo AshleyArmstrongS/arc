02e2fb87dfb52a413e5bdcc479a0901618d1e920
AshleyArmstrongS <D00213195@student.dkit.ie>
2019-11-30 17:51:52 +0000

    Adapt the home controller code to Profile takes the same code as Home and adapts it to get a diffrent user than Session

--- Client/controllers/Profile.php
+++ Client/controllers/Profile.php
@@ -1,12 +1,24 @@
 <?php error_reporting(E_ALL ^ E_NOTICE); ?>
 <?php return function ($req, $res) {
     require('./models/User.php');
+    require('./models/Rating.php');
     $req->sessionStart();
-    $db = \Rapid\Database::getPDO();
-    $user_id = $req->query('user_id');
-    //$user_id = 1;
-    $user = User::getUserByUser_ID($user_id, $db);
 
+    if ($_SESSION['LOGGED_IN'] === TRUE) {
+
+        $db = \Rapid\Database::getPDO();
+        $user_id = $req->query('user_id');
+        $user = User::getUserByUser_ID($user_id, $db);
+        $ratings = Rating::getRatingsByDriver_id($user_id, $db);
+        
+        $res->render('main', 'profile', [
+            'pageTitle' => 'Home',
+            'user' => $user,
+            'ratings' =>$ratings
+        ]);
+    } else {
+        $res->render('main', '404', []);
+    }
     $res->render('main', 'profile', [
         'pageTitle' => 'Profile',
         'user' => $user
