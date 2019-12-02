83b490e40c12ecb11a49e5a23f34232c8f129fc6
Conor Clarke <conorjamesclarke@gmail.com>
2019-12-02 16:24:07 +0000

    Changed layout of card footer

--- Client/templates/views/login.php
+++ Client/templates/views/login.php
@@ -2,7 +2,7 @@
 
 <style>
 body {
-  background-image: url("../Client/assets/images/background-login1.jpg");
+  background-image: url("../Client/assets/images/test.png");
   background: cover;
   overflow: hidden;
 }
@@ -93,20 +93,18 @@ img.avatar {
             </div>
             <input type="password" class="form-control" name='password' value='<?= $password ?>' placeholder="password">
           </div>
-          <div class="row align-items-center remember">
-            <input type="checkbox">Remember Me
-          </div>
+          
           <div class="form-group">
             <input type="submit" value="Login" class="btn float-right login_btn">
           </div>
 
         </form>
       </div>
-      <div class="card-footer" id="footer-name">
+      <div class="footer" id="footer-name">
         <div class="d-flex justify-content-center links">
-          Don't have an account?<a id="regLink" href='<?= SITE_BASE_DIR ?>/userType'>Sign Up</a>
+          Don't have an account?<a id="regLink" href='<?= SITE_BASE_DIR ?>/register'>Sign Up</a>
         </div>
-        <div class="d-flex justify-content-center">
+        <div class="d-flex justify-content-center links">
           <a id="regLink" href="<?= SITE_BASE_DIR ?>/404">Forgot your password?</a>
         </div>
       </div>
