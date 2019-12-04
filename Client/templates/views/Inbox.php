dd40d7d5d32a14b65286a6ec94455a69e2dd17fb
Conor Clarke <conorjamesclarke@gmail.com>
2019-12-04 12:14:21 +0000

    Changed the style of Inbox view

--- Client/templates/views/Inbox.php
+++ Client/templates/views/Inbox.php
@@ -1,166 +1,100 @@
-<!-- <script>
-  //Reference - https://www.w3schools.com/howto/howto_js_tabs.asp
-  function openInbox(evt, inboxBlock) {
-  // Declare all variables
-    var i, tabcontent, tablinks;
-
-    // Get all elements with class="tabcontent" and hide them
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
-
-    // Get all elements with class="tablinks" and remove the class "active"
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-
-    // Show the current tab, and add an "active" class to the button that opened the tab
-    document.getElementById(inboxBlock).style.display = "block";
-    evt.currentTarget.className += " active";
-  }
-
-  document.getElementById("defaultOpen").click();
-
-</script> -->
-
-
 <style>
-  /* https://stackoverflow.com/questions/41674548/how-to-make-html-table-columns-resizable/41675248 */
-  /* table {
-  border-collapse: collapse;
-  border-spacing: 0px;
-}
-td {
-  border: 2px solid black;
-  padding: 5px;
-  margin: 5px;
-  overflow: auto;
+body {
+  overflow: hidden;
 }
 
-
-div {
-  resize: both;
-  overflow: auto;
-  width: auto;
-  height: auto;
-  margin: 0px;
-  padding: 0px;
-  border: 1px solid black; 
-  display:block;
+p {
+  margin: 0;
+  padding: 0;
 }
 
-td div {
-  border: 0;
-  width: auto;
-  height: auto;
-  border: 1px solid black;
-  display:block;
-}  */
-  p {
-    margin: 0;
-    padding: 0;
-  }
+.card {
+  background-color: #fbeee1;
+  opacity: 0.95;
+  margin:0;
+  padding:0;
+  border-radius: 0;
+}
 
+.card:nth-child(odd){
+  background-color: ##F5F5F5;
+}
 
-  p:nth-child(even) {
-    background: #F8F8FF
-  }
+.card:nth-child(even){
+  background-color: #66CCCC;
+}
 
-  p:nth-child(odd) {
-    background: #E6E6FA
-  }
+a {
+  color: #0000FF;
+  text-decoration: none;
+  opacity: 0.55;
+  margin:0;
+  padding:0;
+}
 
+a:hover {
+  
+  background-color: white;
+  color: blue;
+  opacity: 1;
+}
 
-  a {
-    color: #0000FF;
-    text-decoration: none;
-  }
+#wrapper {
+  background: #FFF;
+  margin:0;
+  padding:0;
+}
 
-  a:hover {
-    text-decoration: underline;
-  }
+#chatbox {
+  font: 12px Helvetica;
+  background: #FFF;
+  text-align: left;
+  overflow-y: scroll;
+  max-height: 400px;
+  margin:0;
+  padding:0;
+}
 
-  #wrapper {
-    margin: 0 auto;
-    padding: 0px 25px 25px 25px;
-    background: #FFF;
-  }
-
-  #chatbox {
-    font: 12px Helvetica;
-    width: 90%;
-    padding: 12px 20px;
-    margin: 10px 10px;
-    background: #F5FFFA;
-    color: black;
-    display: inline-block;
-    border: 1px solid #ccc;
-    border-radius: 4px;
-    box-sizing: border-box;
-  }
-
-  #top {
-    padding: 12.5px 25px 12.5px 25px;
-  }
 </style>
 
-<div class="container">
+
   <div class="col-sm-10">
     <h1>Inbox</h1>
   </div>
-
-  <!-- <div class="tab">
-    <button class="tablinks" onclick="openInbox(event, 'Messages')" id="defaultOpen">Messages</button>
-    <button class="tablinks" onclick="openInbox(event, 'Lifts')">Lifts</button>
-  </div> -->
-
-  <!-- Tab content -->
-  <!-- <div id="Messages" class="tabcontent" class="active"> -->
+  <div class="col-sm-10">
+    <span>Click on message to open chat!</span>
+  </div>
   <div id="wrapper">
-    <div class="top"></div>
-    <div id="chatbox">
+    
+    <div id="chatbox" class="card-col">
 
       <?php foreach ($locals['message_info'] as $message) {
 
         if ($message['message'] !== NULL) { ?>
 
-          <?php
-              if (strtotime($message['time_sent']) <= time() - (60 * 60 * 24)) {
-                $regEx = '/(\d{4})-(\d{2})-(\d{2}) /';
-                preg_match($regEx, $message['time_sent'], $result);
-              } else {
-                $regEx = '/ (\d{2}):(\d{2})/';
-                preg_match($regEx, $message['time_sent'], $result);
-              }
-              ?>
-
+      <?php
+        if (strtotime($message['time_sent']) <= time() - (60 * 60 * 24)) {
+          $regEx = '/(\d{4})-(\d{2})-(\d{2}) /';
+          preg_match($regEx, $message['time_sent'], $result);
+        } else {
+          $regEx = '/ (\d{2}):(\d{2})/';
+          preg_match($regEx, $message['time_sent'], $result);
+        }
+        ?>
+        <div class="card">
+      <a href='<?= SITE_BASE_DIR ?>/message?to_id=<?= $message['group_id']; ?>'>
+        
           <i style="font-weight:bold; color:black;"><?= $message['name']; ?>: </i>
-
           <?= $message['message']; ?>
           <br>
-
-          <a href='<?= SITE_BASE_DIR ?>/message?to_id=<?= $message['group_id']; ?>'>View Chat</a>
-          <br>
-          <i style="color: #B0C4DE">(<?= $result[0]; ?>)</i>
-
-
+          <i style="color: black; font-weight: bold;" >(<?= $result[0]; ?>)</i>
+        
+      </a>
+      </div>
 
 
       <?php }
       } ?>
 
     </div>
-    <!-- </div> -->
-
-    <!-- <div id="Lifts" class="tabcontent"> -->
-    <!-- <ul class="list-group">
-      <li class="list-group-item text-muted">Inbox</li>
-      <li class="list-group-item text-left"><a href="#" class="pull-left">lifts goes here</a>  Maybe insert day here -->
-    </li>
-    <!-- <li class="list-group-item text-left"><a href="#" class="pull-left">lifts goes here</a>  Maybe insert the timetable here -->
-    </li>
-    <!--</ul>
   </div>  
-</div> -->
