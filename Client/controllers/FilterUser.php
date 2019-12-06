6e07816ae4a27b87ac5fcf2be5fa15e8507c93c3
Rachel Callaghan <rachelcallaghan81@gmail.com>
2019-12-06 13:24:37 +0000

    Remove map from controller

--- Client/controllers/FilterUser.php
+++ Client/controllers/FilterUser.php
@@ -1,14 +1,3 @@
-<style>
-    #map_wrapper {
-        height: 400px;
-    }
-
-    #map_canvas {
-        width: 300%;
-        height: 100%;
-    }
-</style>
-
 <?php error_reporting(E_ALL ^ E_NOTICE); ?>
 <?php return function ($req, $res) {
 
@@ -60,75 +49,7 @@
                         </div>
                     </div>
                 </div>
-        <?php }
-                }
-                ?>
-        <div id="map_wrapper">
-            <div id="map_canvas" class="mapping"></div>
-        </div>
-        <script>
-            //reference - https://wrightshq.com/playground/placing-multiple-markers-on-a-google-map-using-api-3/
-            jQuery(function($) {
-                // Asynchronously Load the map API 
-                var script = document.createElement('script');
-                script.src = "//maps.googleapis.com/maps/api/js?sensor=false&callback=initialize";
-                document.body.appendChild(script);
-            });
-
-            function initialize() {
-                var map;
-                var bounds = new google.maps.LatLngBounds();
-                var mapOptions = {
-                    mapTypeId: 'roadmap'
-                };
-
-                // Display a map on the page
-                map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
-                map.setTilt(45);
-
-                // Multiple Markers
-                var markers = [
-                    ['CurrentUser', 52.099633, -0.144755],
-                    ['User 2', 51.499633, -0.124755]
-                ];
-
-
-                // Display multiple markers on a map
-                var infoWindow = new google.maps.InfoWindow(),
-                    marker, i;
-
-                // Loop through our array of markers & place each one on the map  
-                for (i = 0; i < markers.length; i++) {
-                    var position = new google.maps.LatLng(markers[i][1], markers[i][2]);
-                    bounds.extend(position);
-                    marker = new google.maps.Marker({
-                        position: position,
-                        map: map,
-                        title: markers[i][0]
-                    });
-
-                    // Allow each marker to have an info window    
-                    google.maps.event.addListener(marker, 'click', (function(marker, i) {
-                        return function() {
-                            infoWindow.setContent(infoWindowContent[i][0]);
-                            infoWindow.open(map, marker);
-                        }
-                    })(marker, i));
-
-                    // Automatically center the map fitting all markers on the screen
-                    map.fitBounds(bounds);
-                }
-
-                // Override our map zoom level once our fitBounds function runs (Make sure it only runs once)
-                var boundsListener = google.maps.event.addListener((map), 'bounds_changed', function(event) {
-                    this.setZoom(14);
-                    google.maps.event.removeListener(boundsListener);
-                });
-
-            }
-        </script>
-
-
-
-<?php    }
+<?php }
+        }
+    }
 } ?>
