21cdc2303372a11034de518a33190d61ddac3246
Aisling Cunningham <Aislingc98@icloud.com>
2019-12-06 13:32:20 +0000

    Added logo to nav bar
    The logo now appears on the nav bar beside the name.

--- Client/templates/layouts/main.php
+++ Client/templates/layouts/main.php
@@ -47,11 +47,15 @@
     margin: 0;
     padding: 0;
   }
+
   </style>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light" style="opacity: 1;">
+  <nav class="navbar navbar-expand-lg navbar-light bg-light" style="opacity 1;">
 
     <div class="container">
-      <div class="navbar-brand" href="">GoCollege</div>
+    
+    <div class="navbar-brand"><img src="../Client/assets/images/GoCollegeLogo.png" height= 70px;></div>
+      <div class="navbar-brand" href="">
+      <div class="font-weight-bold"> Go College</div></div>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample07" aria-controls="navbarsExample07" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
@@ -70,6 +74,8 @@
               <a class="nav-link" href="<?= SITE_BASE_DIR ?>/inbox"><span class="far fa-envelope"></span> Inbox</a>
             </li>
           <?php } ?>
+          </ul>
+          <ul class="form-inline my-2 my-md-0">
           <?php if (!($_SESSION['LOGGED_IN'] === TRUE)) { ?>
             <li class="nav-item active">
               <a class="nav-link" href="<?= SITE_BASE_DIR ?>/login"><span class="fas fa-users"></span> Log in</a>
@@ -78,14 +84,12 @@
               <a class="nav-link" href="<?= SITE_BASE_DIR ?>/register"><span class="fas fa-user-cog"></span> Register</a>
             </li>
           <?php } ?>
-        </ul>
+          </ul>
         <form class="form-inline my-2 my-md-0">
           <?php if ($_SESSION['LOGGED_IN'] === TRUE) { ?>
 
           <a class="nav-link" ><span class="far fa-user"></span> Hi,<?=$_SESSION['Name'];?></a>
-          <a class="nav-link"  href="<?= SITE_BASE_DIR ?>/logout"><span
-              class="fas fa-sign-out-alt"></span> Logout</a>
-
+          <a class="nav-link"  href="<?= SITE_BASE_DIR ?>/logout"><span class="fas fa-sign-out-alt"></span> Logout</a>
           <?php } ?>
         </form>
       </div>
