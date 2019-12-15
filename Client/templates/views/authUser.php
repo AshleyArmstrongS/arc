3a29ff5ea7e57db42ec76c13f436ba6d922f7a74
Aisling Cunningham <Aislingc98@icloud.com>
2019-12-15 09:16:49 +0000

    Update view to display error messages

--- Client/templates/views/authUser.php
+++ Client/templates/views/authUser.php
@@ -9,7 +9,9 @@
             
             <div class="card-body">
                 <form id='authUser' action='' method='post'>
-            
+                <?php foreach ($locals['form_error_messages'] as $errors) { ?>
+                        <p class='error'><?= $errors ?></p>
+                    <?php } ?>
             <div class="form-group">
                         <input type="text" name="code" id="code" class="form-control" placeholder="Authentication Code">
             </div>
